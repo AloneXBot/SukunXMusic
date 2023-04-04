2b534907e0567084f0de8e41bc48d8efc3da11d1
TeamSukun <115199434+TeamSukun@users.noreply.github.com>
2023-04-04 11:30:41 +0530

    Update autoend.py

--- SukunXMusic/plugins/sudo/autoend.py
+++ SukunXMusic/plugins/sudo/autoend.py
@@ -22,7 +22,7 @@ AUTOEND_COMMAND = get_command("AUTOEND_COMMAND")
 
 @app.on_message(filters.command(AUTOEND_COMMAND) & SUDOERS)
 async def auto_end_stream(client, message):
-    usage = "**Usage:**\n\n/autoend [enable|disable]"
+    usage = "**ᴜsᴀɢᴇ:**\n\n/autoend [enable|disable]"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     state = message.text.split(None, 1)[1].strip()
@@ -30,7 +30,7 @@ async def auto_end_stream(client, message):
     if state == "enable":
         await autoend_on()
         await message.reply_text(
-            "Auto End Stream Enabled.\n\nBot will leave voice chat automatically after 3 mins if no one is listening with a warning message.."
+            "ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴇɴᴀʙʟᴇᴅ.\n\nBot will leave voice chat automatically after 3 mins if no one is listening with a warning message.."
         )
     elif state == "disable":
         await autoend_off()
