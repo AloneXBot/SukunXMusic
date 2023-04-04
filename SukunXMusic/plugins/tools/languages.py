d3e720f921f0a22712800f0adfa7dc4832aaafa4
TeamSukun <115199434+TeamSukun@users.noreply.github.com>
2023-04-04 17:52:14 +0530

    Update languages.py

--- SukunXMusic/plugins/tools/languages.py
+++ SukunXMusic/plugins/tools/languages.py
@@ -86,16 +86,16 @@ async def language_markup(client, CallbackQuery, _):
     old = await get_lang(CallbackQuery.message.chat.id)
     if str(old) == str(langauge):
         return await CallbackQuery.answer(
-            "You're already on same language", show_alert=True
+            "ᴀʟʀᴇᴀᴅʏ sᴀᴍᴇ ʟᴀɴɢᴜᴀɢᴇ ᴘᴇ ʜᴏ ʙᴀʙʏ @TeamSukun", show_alert=True
         )
     try:
         _ = get_string(langauge)
         await CallbackQuery.answer(
-            "Successfully changed your language.", show_alert=True
+            "ʟᴀɴɢᴜᴀɢᴇ ᴄʜᴀɴɢᴇ ʜᴏ ɢʏᴀ ʙᴀʙʏ.", show_alert=True
         )
     except:
         return await CallbackQuery.answer(
-            "Failed to change language or Language under update.",
+            "ʟᴀɴɢᴜᴀɢᴇ ᴄʜᴀɴɢᴇ ɴᴏɪ ᴋᴀʟ ᴘᴀᴀ ʟᴀʜɪ ʙᴀʙʏ.",
             show_alert=True,
         )
     await set_lang(CallbackQuery.message.chat.id, langauge)
