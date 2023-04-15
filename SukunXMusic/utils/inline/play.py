1ee3962685baddbb57278c7aa65fb7e0d5bf4baf
Team Sukun <115199434+TeamSukun@users.noreply.github.com>
2023-04-15 13:44:06 +0530

    Update play.py

--- SukunXMusic/utils/inline/play.py
+++ SukunXMusic/utils/inline/play.py
@@ -15,8 +15,8 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
-    anon = math.floor(percentage)
-    if 0 < anon <= 10:
+    SukunXMusic = math.floor(percentage)
+    if 0 < SukunXMusic <= 10:
         bar = "◉—————————"
     elif 10 < SukunXMusic < 20:
         bar = "—◉————————"
@@ -73,8 +73,8 @@ def telegram_markup_timer(_, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
-    anon = math.floor(percentage)
-    if 0 < anon <= 10:
+    SukunXMusic = math.floor(percentage)
+    if 0 < SukunXMusic <= 10:
         bar = "◉—————————"
     elif 10 < SukunXMusic < 20:
         bar = "—◉————————"
